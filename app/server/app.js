231e2cfaf72100d1896b45becea452c138887da0
Duncan Walker <duncan.walker01@bbc.co.uk>
2018-01-18 16:47:45 +0000

    add facebook hooks

--- app/server/app.js
+++ app/server/app.js
@@ -1,7 +1,15 @@
 import path from 'path';
 import express from 'express';
+import bodyParser from 'body-parser';
 
 const app = express();
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json());
+
 
 const publicPath = express.static(path.join(__dirname, '../'));
 const indexPath = path.join(__dirname, '../index.html');
@@ -11,5 +19,15 @@ app.use(publicPath);
 app.get('/', (req, res) => {
     res.sendFile(indexPath);
 })
+
+app.get('/fb-hook', (req, res) => {
+    console.log('get',req.body);
+    res.status(200).send();
+});
+
+app.post('/fb-hook', (req, res) => {
+    console.log('post',req.body);
+    res.status(200).send();
+});
 
 export default app;
