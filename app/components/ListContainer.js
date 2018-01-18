6447870506512a8c2d36fcbd21290dc297255218
Duncan Walker <duncan.walker01@bbc.co.uk>
2018-01-18 20:02:18 +0000

    WIP connect to socket

--- app/components/ListContainer.js
+++ app/components/ListContainer.js
@@ -2,19 +2,23 @@ import React from 'react'
 import { Button, Icon, Item, Label } from 'semantic-ui-react'
 import List from './List';
 import { markAsRead, receiveMessage } from './state';
+import createClient from './socket-client';
 
 class ListContainer extends React.PureComponent {
   constructor() {
-    super()
+    super();
     this.state = {
       messages: [],
     };
+
+    this.client = createClient();
+    this.client.onMessage(message => this.setState(receiveMessage(message)));
   }
 
   render() {
     return <List
-      markAsRead={mid => this.setState(markAsRead(mid))}
-      recieveMessage={message => this.setState(receiveMessage(message))}
+      markAsRead={mid => {}}
+      sendMessage={this.client.sendMessage}
     />
   }
 
