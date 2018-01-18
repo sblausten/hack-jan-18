e1fca715c512eb22fdb7bf950345ebb652b6d901
Samuel Blausten <samuel.blausten@bbc.co.uk>
2018-01-18 17:45:10 +0000

    Cleaned up template code

--- app/server/app.js
+++ app/server/app.js
@@ -24,73 +24,51 @@
 const PAGE_ACCESS_TOKEN = 'pEAACZCH66FWwUBAPcWsLi2nIIYrPsBZBwbZCfA1sGsidZBZCxS5NQtj74w0MHLzWoIvq9UjPywtVBL7TyP4K6xqTrMHJhcDx5pCn98ynuRHB9Yd3wpVcqOZAm0gn0MHFUmwx5t8oY7HnhK3tK3fgQs5zXR83wJ9JeL6FybOoinSgwZDZD';
 const VERIFY_TOKEN = "people-wire";
 
-// Imports dependencies and set up http server
 const
     request = require('request'),
     express = require('express'),
     body_parser = require('body-parser'),
     app = express().use(body_parser.json()); // creates express http server
 
-// Accepts POST requests at /webhook endpoint
 app.post('/fb-hook', (req, res) => {
 
-    // Parse the request body from the POST
     let body = req.body;
 
-    // Check the webhook event is from a Page subscription
     if (body.object === 'page') {
-
         body.entry.forEach(function(entry) {
-
-            // Gets the body of the webhook event
             let webhook_event = entry.messaging[0];
             console.log(webhook_event);
 
-
-            // Get the sender PSID
             let sender_psid = webhook_event.sender.id;
             console.log('Sender ID: ' + sender_psid);
 
-            // Check if the event is a message or postback and
-            // pass the event to the appropriate handler function
             if (webhook_event.message) {
                 handleMessage(sender_psid, webhook_event.message);
             } else if (webhook_event.postback) {
-
                 handlePostback(sender_psid, webhook_event.postback);
             }
 
         });
-        // Return a '200 OK' response to all events
         res.status(200).send('EVENT_RECEIVED');
 
     } else {
-        // Return a '404 Not Found' if event is not from a page subscription
         res.sendStatus(404);
     }
 
 });
 
-// Accepts GET requests at the /webhook endpoint
 app.get('/fb-hook', (req, res) => {
 
-    // Parse params from the webhook verification request
     let mode = req.query['hub.mode'];
     let token = req.query['hub.verify_token'];
     let challenge = req.query['hub.challenge'];
 
-    // Check if a token and mode were sent
     if (mode && token) {
-
-        // Check the mode and token sent are correct
         if (mode === 'subscribe' && token === VERIFY_TOKEN) {
-
-            // Respond with 200 OK and challenge token from the request
             console.log('WEBHOOK_VERIFIED');
             res.status(200).send(challenge);
 
         } else {
-            // Responds with '403 Forbidden' if verify tokens do not match
             res.sendStatus(403);
         }
     }
@@ -99,7 +77,6 @@ app.get('/fb-hook', (req, res) => {
 function handleMessage(sender_psid, received_message) {
     let response;
 
-    // Checks if the message contains text
     if (received_message.text) {
         // Create the payload for a basic text message, which
         // will be added to the body of our request to the Send API
@@ -136,28 +113,23 @@ function handleMessage(sender_psid, received_message) {
         }
     }
 
-    // Send the response message
     callSendAPI(sender_psid, response);
 }
 
 function handlePostback(sender_psid, received_postback) {
     console.log('ok')
     let response;
-    // Get the payload for the postback
     let payload = received_postback.payload;
 
-    // Set the response based on the postback payload
     if (payload === 'yes') {
         response = { "text": "Thanks!" }
     } else if (payload === 'no') {
         response = { "text": "Oops, try sending another image." }
     }
-    // Send the message to acknowledge the postback
     callSendAPI(sender_psid, response);
 }
 
 function callSendAPI(sender_psid, response) {
-    // Construct the message body
     let request_body = {
         "recipient": {
             "id": sender_psid
@@ -165,7 +137,6 @@ function callSendAPI(sender_psid, response) {
         "message": response
     }
 
-    // Send the HTTP request to the Messenger Platform
     request({
         "uri": "https://graph.facebook.com/v2.6/me/messages",
         "qs": { "access_token": PAGE_ACCESS_TOKEN },
