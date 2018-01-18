ae4cc7157da27730622a84488b3dafe2fc14f8ee
Duncan Walker <duncan.walker01@bbc.co.uk>
2018-01-18 17:28:13 +0000

    add facebook verfication code

--- app/server/app.js
+++ app/server/app.js
@@ -21,11 +21,45 @@ app.get('/', (req, res) => {
 })
 
 app.get('/fb-hook', (req, res) => {
-    console.log('get',req.body);
+    console.log('get');
+    // Your verify token. Should be a random string.
+    let VERIFY_TOKEN = "people-wire"
+
+    // Parse the query params
+    let mode = req.query['hub.mode'];
+    let token = req.query['hub.verify_token'];
+    let challenge = req.query['hub.challenge'];
+
+    // Checks if a token and mode is in the query string of the request
+    if (mode && token) {
+
+        // Checks the mode and token sent is correct
+        if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+
+            // Responds with the challenge token from the request
+            console.log('WEBHOOK_VERIFIED');
+            res.status(200).send(challenge);
+
+        } else {
+            // Responds with '403 Forbidden' if verify tokens do not match
+            res.sendStatus(403);
+        }
+    }
     res.status(200).send();
 });
 
 app.post('/fb-hook', (req, res) => {
+    const {body} = req;
+    if (body.object === 'page') {
+        body.entry.forEach(function(entry) {
+            let webhook_event = entry.messaging[0];
+            console.log(webhook_event);
+        });
+
+        // Returns a '200 OK' response to all requests
+        res.status(200).send('EVENT_RECEIVED');
+    }
+
     console.log('post',req.body);
     res.status(200).send();
 });
