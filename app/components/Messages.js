67e491753bfb13ad20777d78b209686a6e631b93
Phoebe Greig <greigp02@mc-s107333.local>
2018-01-19 00:15:22 +0000

    customised message send

--- app/components/Messages.js
+++ app/components/Messages.js
@@ -12,13 +12,13 @@ function Messages(props) {
     const { id } = sender;
 
     return (    
-        <div className='Messages'>
+        <div className={`Messages Message-${id}`}>
             <span>Contact Source: </span>
             <button className='btn btn-primary read' onClick={() => sendMessage({ sid: id, text: message.read })} >Read</button>
             <button className=' btn btn-primary investigating' onClick={() => sendMessage({ sid: id, text: message.investigating })}>Investigating</button>
             <button className=' btn btn-primary writing' onClick={() => sendMessage({ sid: id, text: message.writing })}>Editing</button>
             <button className=' btn btn-primary published' onClick={() => sendMessage({ sid: id, text: message.published })}>Published</button>
-            <input type='text'/><button  className='btn btn-dark'type='submit'>Send Custom Msg</button>  
+            <input id={`free-text-${id}`} type='text'/><button  className='btn btn-dark'type='submit' onClick={() => sendMessage({ sid: id, text: document.getElementById(`free-text-${id}`).value })}>Send Custom Msg</button>  
         </div>
             
     )
